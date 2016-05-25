0aed439bc6489889fc37c414abcc307544aa299a
Marius-Sorin Stratulat <smarius.sorin@yahoo.com>
2016-05-26 01:25:12 +0300

    --

--- domain-item/lib/businessRule.js
+++ domain-item/lib/businessRule.js
@@ -1,18 +1,6 @@
 module.exports = require('cqrs-domain').defineBusinessRule({
   name: 'checkForError'
 }, function (changed, previous, events, command) {
-  
-  console.log("------changed------");
-  console.log(changed);
-  console.log("------changed payload-----");
-  console.log(changed.uncommittedEvents);
-  console.log("------previous------");
-  console.log(previous);
-  console.log("------events------");
-  console.log(events);
-  console.log("------command------");
-  console.log(command);
-
   if (changed.get('userId') == null) {
       throw new Error('userId cannot be empty');
   }
