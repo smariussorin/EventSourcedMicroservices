7a1d2aac4b848bab8ed2daf325dc11cd26ed4efa
Marius-Sorin Stratulat <smarius.sorin@yahoo.com>
2016-05-27 19:28:49 +0300

    Removed duplicate file

--- host/app/routes/routes.js
+++
@@ -1,4 +0,0 @@
-exports.actions = function(app, options, repository) {
-	require('./routes/account').actions(app, options, repository);
-	require('./routes/item').actions(app, options, repository);
-};
