ff95ffc23f8494fade3898296cb4f4c110b574ea
Marius-Sorin Stratulat <smarius.sorin@yahoo.com>
2016-06-14 02:59:33 +0300

    Minor css + js improvements

--- host/public/scripts/controllers/overview.js
+++ host/public/scripts/controllers/overview.js
@@ -127,9 +127,32 @@ app
     $scope.chart = [];
 
     var generateColor = function(){
-      return '#' + (Math.random().toString(16) + "000000").substring(2,8);
+       var r = (Math.round(Math.random()* 127) + 127).toString(16);
+       var g = (Math.round(Math.random()* 127) + 127).toString(16);
+       var b = (Math.round(Math.random()* 127) + 127).toString(16);
+       return '#' + r + g + b;
     }
     
+    function shadeColor(color, percent) {
+        var R = parseInt(color.substring(1,3),16);
+        var G = parseInt(color.substring(3,5),16);
+        var B = parseInt(color.substring(5,7),16);
+
+        R = parseInt(R * (100 + percent) / 100);
+        G = parseInt(G * (100 + percent) / 100);
+        B = parseInt(B * (100 + percent) / 100);
+
+        R = (R<255)?R:255;  
+        G = (G<255)?G:255;  
+        B = (B<255)?B:255;  
+
+        var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+        var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+        var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+        return "#"+RR+GG+BB;
+    }
+
     var getCategoriesPromise = categoryRepository.query().$promise;
     getCategoriesPromise
     .then(function (result) {
@@ -161,7 +184,7 @@ app
 
         $scope.chart[key] = {};
         $scope.chart[key].color = generateColor();
-        $scope.chart[key].highlight = generateColor();
+        $scope.chart[key].highlight = shadeColor($scope.chart[key].color, -15);
         $scope.chart[key].label = val.name;
         $scope.chart[key].value = quantity;
       });
