88d9a17942c7e8f3a1f4f6fa5642ccf2b27c2661
Marius-Sorin Stratulat <smarius.sorin@yahoo.com>
2016-06-14 22:55:07 +0300

    Renamed host

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
-start node domain-category/server.js
-start node domain-product/server.js
-start node domain-order/server.js
-start node saga-category/server.js
-start node saga-product/server.js
-start node host/server.js
+start node microservice-category/server.js
+start node microservice-product/server.js
+start node microservice-order/server.js
+start node microservice-saga-category/server.js
+start node microservice-saga-product/server.js
+start node web-host/server.js
