e7679bdd658331ceb6bcd93560b6c9290bb8e08f
Marius-Sorin Stratulat <smarius.sorin@yahoo.com>
2016-06-14 03:00:46 +0300

    Changed Start bat

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 start node domain-category/server.js
-start node domain-order/server.js
 start node domain-product/server.js
-start node saga/server.js
+start node domain-order/server.js
+start node saga-category/server.js
+start node saga-product/server.js
 start node host/server.js
